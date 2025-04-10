399527f234fe5fb1414d12798342c8c8cda9d7fa
Clau <cladiaz@uade.edu.ar>
2025-04-10 14:56:38 -0300

    wwww

--- Cargadedatos.py
+++ Cargadedatos.py
@@ -1,21 +1,41 @@
 
 paciente = []
 
-proceso = int(input("Ingrese su dni o -1 para terminar: "))
-if proceso != -1:
-    while proceso == 0:
-        dni = 0
-        paciente.append(dni)
+def procesoval(a):
+    if a < 10000000000 or a > 99999999999:
+        print("Su dni debe tener exactamente 11 digitos: ")
+        return False
+    return True
+
+def mailval(b):
+    if "@" not in b:
+        print("Su mail no es valido, porfavor ingreselo nuevamente")
+        return False
+    return True
+
+
+proceso = int(input("Ingrese un dni valido o -1 para terminar: "))
+while proceso != -1:
+    while not procesoval(proceso):
+        proceso = int(input("Ingrese un dni con 11 digitos o -1 para terminar: "))
+
+    if proceso != -1:
+        paciente.append(proceso)
         nombyapel = input("Ingrese el nombre y apellido del paciente: ")
         paciente.append(nombyapel)
-        #dni = int(input("Ingrese el dni del paciente: "))
-        #paciente.append(dni)
         mail = input("Ingrese el mail del paciente: ")
-        paciente.append(mail)
-        part = int(input("Ingrese 1 si es un particular o 2 si posee una obra social: "))
+
+    while not mailval(mail):
+        mail = input("Ingrese un mail valido porfavor: ")
+
+    paciente.append(mail)
+    part = int(input("Ingrese 1 si es un particular o 2 si posee una obra social: "))
+    while part > 2 or part < 1:
+        part = int(input("Ingrese un numero entre 1 y 2, 1 si es un particular o 2 si posee obra social: "))
+    else:
         paciente.append(part)
-        proceso = int(input("Ingrese 0 para continuar o cualquier numero para terminar: "))
+
+    proceso = int(input("Ingrese otro DNI o -1 para terminar: "))
 else:
     print(paciente)
-
 
